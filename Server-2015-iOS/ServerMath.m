7f1e383c77945addd6877335b3aad180dbf44cb0
Bryton Moeller <bryton@themoellers.net>
2015-02-01 14:45:33 -0800

    stacking ability of team (cant test because schema is incomplete)

--- Server-2015-iOS/ServerMath.m
+++ Server-2015-iOS/ServerMath.m
@@ -10,6 +10,8 @@
 #import "RealmModels.h"
 #import "UniqueKey.h"
 
+
+
 @implementation ServerMath
 
 //The block returns the datapoint for the match for the team. It always returns a float, e.g. 0.0 is false, 1.0 is true
@@ -40,6 +42,32 @@
         float returnMe = [teamInMatchData.uploadedData[@"agility"] floatValue];
         return returnMe;
     }];
+}
+
+/*
+ STACKING ABILITY
+ 
+ for match in matches:
+	total += (stackPlacingSecurity/10 + reconsStacked*KONSTANT); //stackPlacingSecurity is a super scout thing
+ 
+ stackingAbility = (total/[matches count]) + OTHERKONSTANT(maxStackHeightWithRecon/timeToStackMaxStackHeightWithRecon) + KONSTANDTHREE(maxStackHeightWithOutRecon/timeToStackMaxStackHeightWithOutRecon); //the max stack height and the max stack height times are pit scout things
+*/
+
+#define STACKING_ABILITY_RECONS_STACKED_CONSTANT 1.0
+#define STACKING_ABILITY_CONSTANT_WITH_RECON 1.0
+#define STACKING_ABILITY_CONSTANT_WITHOUT_RECON 1.0
+
+
+-(float)stackingAbilityOfTeam:(Team *)team
+{
+    float part1 = [self averageWithTeam:team WithDatapointBlock:^float(TeamInMatchData *teamInMatchData, Match *match) {
+        float returnMe = ([teamInMatchData.uploadedData[@"stackPlacing"] floatValue]) + ([teamInMatchData.uploadedData[@"numReconsStacked"] floatValue] * STACKING_ABILITY_CONSTANT_WITH_RECON);
+        return returnMe;
+    }];
+    float part2 = (STACKING_ABILITY_CONSTANT_WITH_RECON * [team[@"maxStackHeightWithRecon"] floatValue]/[team[@"timeToStackMaxStackHeightWithRecon"] floatValue]);
+    float part3 = (STACKING_ABILITY_CONSTANT_WITHOUT_RECON * [team[@"maxStackHeightWithOutRecon"] floatValue]/[team[@"timeToStackMaxStackHeightWithOutRecon"] floatValue]);
+
+    return part1 + part2 + part3;
 }
 
 
